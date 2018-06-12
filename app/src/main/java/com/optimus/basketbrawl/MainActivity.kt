2c79f2572abb79888a390ea40f9b4b99efbba367
Jake Jin <jakejin12@gmail.com>
2018-06-12 19:07:33 -0400

    Login Page

--- app/src/main/java/com/optimus/basketbrawl/MainActivity.kt
+++ app/src/main/java/com/optimus/basketbrawl/MainActivity.kt
@@ -2,11 +2,23 @@ package com.optimus.basketbrawl
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SignupButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        LoginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

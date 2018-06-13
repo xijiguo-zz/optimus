06413ec7c87974dc2a09c9a07bcd3bff1513da73
Steven Kong <stevendjkong@gmail.com>
2018-06-12 20:06:19 -0400

    Added homeview with fragments

---
+++ app/src/main/java/com/optimus/basketbrawl/HomeActivity.kt
@@ -0,0 +1,78 @@
+package com.optimus.basketbrawl
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_home.*
+import android.support.v4.view.ViewPager
+import android.view.MenuItem
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private var prevMenuItem: MenuItem? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+//                message.setText(R.string.title_home)
+                pager.currentItem = 0
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+//                message.setText(R.string.title_dashboard)
+                pager.currentItem = 1
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+//                message.setText(R.string.title_notifications)
+                pager.currentItem = 2
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    private fun setupViewPager(viewPager: ViewPager) {
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        val courtFragment = CourtFragment()
+        val gameFragment = CourtFragment()
+        val profileFragment = CourtFragment()
+        adapter.addFragment(courtFragment, "Court")
+        adapter.addFragment(gameFragment, "Game")
+        adapter.addFragment(profileFragment, "Profile")
+        viewPager.adapter = adapter
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        setupViewPager(pager)
+
+        pager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (prevMenuItem != null) {
+                    prevMenuItem!!.isChecked = false
+                } else {
+                    navigation.menu.getItem(0).isChecked = false
+                }
+                navigation.menu.getItem(position).isChecked = true
+                prevMenuItem = navigation.menu.getItem(position)
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+        })
+    }
+
+
+}
--- app/src/main/java/com/optimus/basketbrawl/LoginActivity.kt
+++ app/src/main/java/com/optimus/basketbrawl/LoginActivity.kt
@@ -23,6 +23,7 @@ import android.widget.TextView
 
 import java.util.ArrayList
 import android.Manifest.permission.READ_CONTACTS
+import android.content.Intent
 
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -266,7 +267,8 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
             showProgress(false)
 
             if (success!!) {
-                finish()
+                val homeviewIntent = Intent(applicationContext, HomeActivity::class.java)
+                startActivity(homeviewIntent)
             } else {
                 password.error = getString(R.string.error_incorrect_password)
                 password.requestFocus()
